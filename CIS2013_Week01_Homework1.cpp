f7c875eb527dac4cf8db532aed4c6e574704e772
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-01-27 20:24:42 -0600

    Initial commit

---
+++ CIS2013_Week01_Homework1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "============================================================";
+	cout << "Hamza Zoumhani";
+	cout << "CIS Student, OSU-OKC";
+	cout << "620 NW 178th St  " << "     P: (405) 318-6470";
+	cout << "Edmond, OK;      " << "     E: hamza.zoumhani@okstate.edu";
+	cout << "                            G: github.com/hamza-zoumhani";
+	cout << "                            T: @hamzazoumhani";
+	cout << "============================================================";
